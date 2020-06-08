9acf49f5a10858ff37a2fa8629375ae2123cc32d
devbodaghe <40855726+devbodaghe@users.noreply.github.com>
2020-06-08 16:15:40 +0100

    api issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 
 
-console.log(process.env.REACT_APP_API_KEY);
 
 
 const key:string = process.env.REACT_APP_API_KEY!;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-require('dotenv').config();
-
-console.log(process.env)
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
