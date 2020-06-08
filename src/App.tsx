c293daaa7d0b22da204c58f0c2baed736df7d0f6
devbodaghe <40855726+devbodaghe@users.noreply.github.com>
2020-06-08 16:04:56 +0100

    new calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 
-const key:string = "AIzaSyDMbJoOSv1cbnHZ2mWOcjIUj4r4GyGF1Pg";
+
+console.log(process.env.REACT_APP_API_KEY);
+
+
+const key:string = process.env.REACT_APP_API_KEY!;
 
 const App: React.FC = () => {
 
   const [latitude,setLatitude] = useState<number | undefined>(); 
   const [longitude,setLongitude] = useState<number | undefined>(); 
   const [hospitals, setHospitals] = useState<any []>();
+  const [pharmacies, setPharmacies] = useState<any []>();
+  const [clinics, setClinics] = useState<any []>();
+  const [medicalOffices, setMedicalOffices] = useState<any []>();
   const [address,setAddress] = useState<string | undefined>();
   const [radius, setRadius] = useState("5000");
 
@@ -18,10 +25,17 @@ const App: React.FC = () => {
     { label: "20 km", value: 20000 }, 
   ];
   
-  const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';  
-  const url:string = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=hospital&radius=${radius}&key=${key}`;
-  const URL = PROXY_URL + url
+  const PROXY_URL = 'https://secure-dusk-66741.herokuapp.com/';  
+  const url_hospital:string = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=hospital&radius=${radius}&key=${key}`;
+  const url_pharmacy:string = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=pharmacy&radius=${radius}&key=${key}`;
+  const url_clinic:string = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=clinic&inputtype=textquery&fields=formatted_address,name,rating&locationbias=circle:${radius}@${latitude},${longitude}&key=${key}`
+  const url_medicalOffices:string = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=medical%office&inputtype=textquery&fields=formatted_address,name,rating&locationbias=circle:${radius}@${latitude},${longitude}&key=${key}`
   
+  const URL_HOSPITAL = PROXY_URL + url_hospital
+  const URL_PHARMACY = PROXY_URL + url_pharmacy
+  const URL_CLINIC = PROXY_URL + url_clinic
+  const URL_MEDICALOFFICE = PROXY_URL + url_medicalOffices
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition( position => {
@@ -41,11 +55,28 @@ const App: React.FC = () => {
     .then(response => response.json())
     .then(data => setAddress(data.results[0].formatted_address))
   }
-  const gethospitals = ():void => {
-    fetch(URL)
+  const getPharmacies = (): void => {
+    fetch(URL_PHARMACY)
       .then(res => res.json())
-      .then(data => setHospitals(data.results))
+      .then(data => setPharmacies(data.results))
   }
+  const gethospitals = ():void => {
+    fetch(URL_HOSPITAL)
+      .then(res => res.json())
+      .then(data => setHospitals(data.candidates))
+  }
+  const getClinics = ():void => {
+    fetch(URL_CLINIC)
+      .then(res => res.json())
+      .then(data => setClinics(data.results))
+  }
+
+  const getMedicalOffice = ():void => {
+    fetch(URL_MEDICALOFFICE)
+      .then(res => res.json())
+      .then(data => setMedicalOffices(data.candidates))
+  }
+  
   return (
     <div className="container">
       <h1 className="heading">covid hospital locator</h1>
@@ -67,10 +98,27 @@ const App: React.FC = () => {
           onClick={gethospitals}
         >
           Show Hospitals
+        </button > 
+        <button 
+        className = "btn__hospital"
+        onClick = {getClinics}>
+          Show Clinics
+        </button>
+        <button className = "btn__hospital"
+        onClick = {getMedicalOffice}
+
+        >
+          Show Medical Offices
+        </button>
+        <button className="btn__hospital"
+          onClick = {getPharmacies}
+        >
+          Show Pharmacies
         </button>
       </div>
       {address && <p className = "user--address"> User address: {address}</p>}
-      {
+      <div>
+       {
         hospitals && 
         hospitals.map(result => (
           <div 
@@ -84,6 +132,59 @@ const App: React.FC = () => {
           </div>
         ))
       }
+      </div>     
+      
+      <div>
+      {
+        pharmacies && 
+        pharmacies.map(result => (
+          <div 
+            className="hospitals"
+            key={result.id}
+          >
+            
+            <p>Pharmacy Name: {result.name}</p>
+            <p>Pharmacy address: {result.vicinity}</p>
+            <p>Operational Status: {result.business_status}</p>
+          </div>
+        ))
+      }
+      </div>
+      <div>
+      {
+        clinics && 
+        clinics.map(result => (
+          <div 
+            className="hospitals"
+            key={result.id}
+          >
+            
+            <p>Clinic Name: {result.name}</p>
+            <p>Clinic address: {result.formatted_address}</p>
+            <p>Operational Status: {result.rating}</p>
+          </div>
+        ))
+      }
+      </div>
+      <div>
+      
+      {
+        medicalOffices && 
+        medicalOffices.map(result => (
+          <div 
+            className="hospitals"
+            key={result.id}
+          >
+            
+            <p>Medical Office Name: {result.name}</p>
+            <p>Medical Office address: {result.formatted_address}</p>
+            {/* <p>Operational Status: {result.rating}</p> */}
+          </div>
+        ))
+      } 
+      </div>
+      
+      
     </div>
   )
 }
