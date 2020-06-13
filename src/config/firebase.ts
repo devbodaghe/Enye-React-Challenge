bcb7eddca4739f0f56890b3db5a7f44f453eccf9
David Evbodaghe <david.evbodaghe@aiesec.net>
2020-06-13 19:31:06 +0100

    removed .env dependency

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,12 @@
 let firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REAT_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyANscoLvqTxm3pLyxCuCZTYF6n0LEhfweg",
+    authDomain: "enye-bfbc2.firebaseapp.com",
+    databaseURL: "https://enye-bfbc2.firebaseio.com",
+    projectId: "enye-bfbc2",
+    storageBucket: "enye-bfbc2.appspot.com",
+    messagingSenderId: "582649263944",
+    appId: "1:582649263944:web:45d9721f43ab6a9457257d",
+    measurementId: "G-Y2DXV0Z2ZP"
 };
 
 export default firebaseConfig;
